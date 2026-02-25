1afccf2e4ce310495547a0e95386fb05c0ec7f26
danielwalser335@gmail.com <danielwalser335@gmail.com>
2026-02-25 10:40:46 +0000

    Created Backend; Edited Form1; Deleted ServerModule1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,5 +7,12 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+    sql = """
+      SELECT f.Name, l.LID, r.Zielort, k.Name, w.Datum, f.Datum, s.Beschreibung
+      FROM Fahrer f 
+      INNER JOIN LKW l ON f.LID = r.LID;
+    """    
+    print(sql)
+
+    anvil.server.call()
     # Any code you write here will run before the form opens.
