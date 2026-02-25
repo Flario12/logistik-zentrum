9387874898c02f47d00ad2279bcc5aa8bce14269
danielwalser335@gmail.com <danielwalser335@gmail.com>
2026-02-25 10:29:44 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,3 +1,8 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.files
+from anvil.files import data_files
 import anvil.server
 import sqlite3
 # This is a server module. It runs on the Anvil server,
