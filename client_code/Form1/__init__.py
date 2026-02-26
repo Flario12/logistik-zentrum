d9166579400462f52408347da08906db3b3aa88d
danielwalser335@gmail.com <danielwalser335@gmail.com>
2026-02-26 11:40:00 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -23,7 +23,6 @@ class Form1(Form1Template):
     print(sql)
     
     return_value = anvil.server.call("query_database_dict", sql)
-    print(return_value)
     self.repeating_panel_1.items = return_value
     # Any code you write here will run before the form opens.
     
