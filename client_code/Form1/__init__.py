13878f9ed494424d974b375c4e3601320ba8ffa5
danielwalser335@gmail.com <danielwalser335@gmail.com>
2026-02-26 10:52:52 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -13,16 +13,11 @@ class Form1(Form1Template):
     sql = """
       SELECT 
         f.Name,
-        l.LID,
-        r.Zielort,
-        k.Name,
-        w.Datum,
-        f.Datum,
-        s.Beschreibung
+        r.Zielort
       FROM Fahrer f 
-      INNER JOIN Route r ON f.LID = r.LID;
+      INNER JOIN Route r ON f.FID = r.RID;
     """    
     print(sql)
 
-    anvil.server.call("query_database", sql)
+    anvil.server.call("query_database_dict", sql)
     # Any code you write here will run before the form opens.
