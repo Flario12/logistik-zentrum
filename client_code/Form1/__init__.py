b191d131a902eed39ccd47f5d657e15fece84a75
danielwalser335@gmail.com <danielwalser335@gmail.com>
2026-02-26 11:47:08 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -18,13 +18,14 @@ class Form1(Form1Template):
         r.Zielort AS Zielort
       FROM Fahrer f 
       LEFT JOIN Fahrt fa ON f.FID = fa.FID
-      LEFT JOIN Route r ON fa.RID = r.RID;
-    """    
+      LEFT JOIN Route r ON fa.RID = r.RID
+      ;
+    """ 
+    
     print(sql)
     
     return_value = anvil.server.call("query_database_dict", sql)
     
-
     # Die Liste dem RepeatingPanel übergeben
     self.repeating_panel_1.items = return_value
     # Any code you write here will run before the form opens.
