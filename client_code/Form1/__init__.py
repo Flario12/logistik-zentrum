d359d60c6fb38f9c3c9f32f1323a801a23ff8d04
danielwalser335@gmail.com <danielwalser335@gmail.com>
2026-02-26 15:21:10 +0000

    Created Form1.Form2; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,7 +15,8 @@ class Form1(Form1Template):
       SELECT 
         f.Name AS Name,
         fa.Datum AS Datum,
-        r.Zielort AS Zielort
+        r.Zielort AS Zielort,
+        r.Startort AS Startort
       FROM Fahrer f 
       LEFT JOIN Fahrt fa ON f.FID = fa.FID
       LEFT JOIN Route r ON fa.RID = r.RID
@@ -29,4 +30,10 @@ class Form1(Form1Template):
     # Die Liste dem RepeatingPanel übergeben
     self.repeating_panel_1.items = return_value
     # Any code you write here will run before the form opens.
+
+  @handle("overview", "click")
+  def overview_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Form1.Form2')
+    pass
     
