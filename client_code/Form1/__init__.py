b7a446203964043929ba4a22c0d826fc44fe3c80
danielwalser335@gmail.com <danielwalser335@gmail.com>
2026-03-04 10:12:15 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -39,6 +39,23 @@ class Form1(Form1Template):
 
   @handle("drop_down_1", "change")
   def drop_down_1_change(self, **event_args):
-    """This method is called when an item is selected"""
+    sql = """
+      SELECT 
+        f.Name AS Name,
+        fa.Datum AS Datum,
+        r.Zielort AS Zielort,
+        r.Startort AS Startort
+      FROM Fahrer f 
+      LEFT JOIN Fahrt fa ON f.FID = fa.FID
+      LEFT JOIN Route r ON fa.RID = r.RID
+      ;
+    """ 
+    
+    return_value = anvil.server.call("query_database_dict", sql)
+    for d in return_value:
+      # d["gebaeude_name"] = self.drop_down_gefaengnisliste.selected_value
+      d["gefaengnis_name"] = self.drop_down_gefaengnisliste.selected_value
+    print(return_value)
+    self.repeating_panel_gebaeude.items = return_value
     pass
     
