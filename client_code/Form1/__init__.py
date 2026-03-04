dea15a8860d4fd592bd6f6c73b919924f4e2ac39
danielwalser335@gmail.com <danielwalser335@gmail.com>
2026-03-04 10:27:32 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -16,10 +16,12 @@ class Form1(Form1Template):
         f.Name AS Name,
         fa.Datum AS Datum,
         r.Zielort AS Zielort,
-        r.Startort AS Startort
+        r.Startort AS Startort,
+        l.Firma AS Firma
       FROM Fahrer f 
       LEFT JOIN Fahrt fa ON f.FID = fa.FID
       LEFT JOIN Route r ON fa.RID = r.RID
+      LEFT JOIN LKW l ON f.FID = l.FID
       ;
     """ 
     
@@ -44,18 +46,20 @@ class Form1(Form1Template):
         f.Name AS Name,
         fa.Datum AS Datum,
         r.Zielort AS Zielort,
-        r.Startort AS Startort
+        r.Startort AS Startort,
+        l.Firma AS Firma
       FROM Fahrer f 
       LEFT JOIN Fahrt fa ON f.FID = fa.FID
       LEFT JOIN Route r ON fa.RID = r.RID
+      LEFT JOIN LKW l ON f.FID = l.FID
       ;
     """ 
     
     return_value = anvil.server.call("query_database_dict", sql)
     for d in return_value:
       # d["gebaeude_name"] = self.drop_down_gefaengnisliste.selected_value
-      d["Startort"] = self.drop_down_gefaengnisliste.selected_value
+      d["Firma"] = self.drop_down_1.selected_value
     print(return_value)
-    self.repeating_panel_gebaeude.items = return_value
+    self.repeating_panel_1.items = return_value
     pass
     
